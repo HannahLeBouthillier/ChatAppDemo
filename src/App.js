d5bc96276488f0b518d4d4565cfe8c99325b179e
HannahLeBouthillier <32823091+HannahLeBouthillier@users.noreply.github.com>
2020-09-03 18:31:49 -0300

    fixing a problem
    fixing a problem revert

--- src/App.js
+++ src/App.js
@@ -8,7 +8,21 @@ function App() {
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
+<<<<<<< HEAD
         <h1>We now have Auth! please freaking work!</h1>
+=======
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+>>>>>>> parent of e43eefe8... Second update
       </header>
       <AmplifySignOut />
     </div>
